7ed63181eeddaabc6c3726f0cc8f41fa6c22fb7b
Kuba PrusakiewiczPC <jakubprusakiewicz@gmail.com>
2020-04-26 18:22:11 +0200

    Initial commit

---
+++ ball_simulation/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ball_simulation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
