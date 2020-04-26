6f9de92bf68f4c586c91602b9261a661db78e794
Kuba PrusakiewiczPC <jakubprusakiewicz@gmail.com>
2020-04-26 20:08:40 +0200

    feat one velocity for all/ minor refactor

--- ball_simulation/MainWindow.xaml.cs
+++ ball_simulation/MainWindow.xaml.cs
@@ -14,49 +14,28 @@ namespace ball_simulation
         DispatcherTimer _timer = new DispatcherTimer();
         private double vx = 50.0;
         private double vy = 50.0;
-        
-
+        public static int numOfBalls = 10;
+        Ellipse[] _balls = new Ellipse[numOfBalls];
         public MainWindow()
         {
-            
-            // var elydupa = new Ellipse();
-            // var elyupa = new Ellipse();
-            
             InitializeComponent();
-            
-            spawnBalls(10);
-            
-            // elydupa.Height = 10;
-            // elydupa.Width = 10;
-            // elydupa.Stroke = Brushes.Blue;
-            // Canvas.SetLeft(elydupa, 5);
-            // Canvas.SetTop(elydupa, 5);
-            // KubasCanvas.Children.Add(elydupa);
-            //
-            
-            // elyupa.Height = 10;
-            // elyupa.Width = 10;
-            // elyupa.Stroke = Brushes.MediumVioletRed;
-            // Canvas.SetLeft(elyupa, 215);
-            // Canvas.SetTop(elyupa, 115);
-            // KubasCanvas.Children.Add(elyupa);
-            
-            
+            spawnBalls();
+
             _timer.Interval = TimeSpan.FromSeconds(0.05);
             _timer.IsEnabled = true;
             _timer.Tick += animation;
 
         }
 
-        public void spawnBalls(int numOfBalls)
+        public void spawnBalls()
         {
-            Ellipse[] _balls = new Ellipse[numOfBalls];
+            
             for (int i = 0; i < numOfBalls-1; i++)
             {
                 _balls[i] = new Ellipse();
                 _balls[i].Width = 10;
                 _balls[i].Height = 10;
-                _balls[i].Fill = Brushes.Brown;
+                _balls[i].Fill = Brushes.Green;
                 Canvas.SetTop(_balls[i], i * 15);
                 Canvas.SetLeft(_balls[i], i * 15);
                 KubasCanvas.Children.Add(_balls[i]);
@@ -64,6 +43,26 @@ namespace ball_simulation
         }
         void animation(object sender, EventArgs e)
         {
+            for (int i = 0; i < numOfBalls-1; i++)
+            {
+                double _x = Canvas.GetLeft(_balls[i]);
+                _x += vx * _timer.Interval.TotalSeconds;
+                if (_x <= 0.0 || _x >= KubasCanvas.ActualWidth - _balls[i].Height)
+                {
+                    vx = -vx;
+                }
+                Canvas.SetLeft(_balls[i], _x);
+            
+                double _y = Canvas.GetTop(_balls[i]);
+                _y += vy * _timer.Interval.TotalSeconds;
+
+                if (_y <= 0.0 || _y >= KubasCanvas.ActualHeight - _balls[i].Height)
+                {
+                    vy = -vy;
+                }
+                Canvas.SetTop(_balls[i], _y);
+            }
+            
             double x = Canvas.GetLeft(ball1);
             x += vx * _timer.Interval.TotalSeconds;
             if (x <= 0.0 || x >= KubasCanvas.ActualWidth - ball1.Height)
@@ -74,7 +73,7 @@ namespace ball_simulation
             
             double y = Canvas.GetTop(ball1);
             y += vy * _timer.Interval.TotalSeconds;
-
+            
             if (y <= 0.0 || y >= KubasCanvas.ActualHeight - ball1.Height)
             {
                 vy = -vy;
